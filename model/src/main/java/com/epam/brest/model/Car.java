9fb79882a007812a54f1e76fc2c526e321e154f6
pavel krauchuk <krauchukp@gmail.com>
2021-11-04 22:45:42 +0300

    multimodel structure (model)

---
+++ model/src/main/java/com/epam/brest/model/Car.java
@@ -0,0 +1,50 @@
+package com.epam.brest.model;
+
+public class Car {
+    private String model;
+    private String color;
+    private Integer yearOfIssue;
+    private Integer mileage;
+    private String carNumber;
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void setYearOfIssue(Integer yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+}
---
+++ model/src/main/java/com/epam/brest/model/Customer.java
@@ -0,0 +1,59 @@
+package com.epam.brest.model;
+
+public class Customer {
+
+    private Integer cusomerId;
+    private String firstName;
+    private String lastName;
+    private Integer numberDriverLicense;
+    private Integer phoneNumber;
+    private String address;
+
+    public Integer getCusomerId() {
+        return cusomerId;
+    }
+
+    public void setCusomerId(Integer cusomerId) {
+        this.cusomerId = cusomerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberDriverLicense() {
+        return numberDriverLicense;
+    }
+
+    public void setNumberDriverLicense(Integer numberDriverLicense) {
+        this.numberDriverLicense = numberDriverLicense;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
